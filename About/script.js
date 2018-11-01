f3b99858000760db87ec130dda5601867a2342bd
jbiscailuz <josefinabiscailuz@gmail.com>
2018-11-01 12:26:39 -0400

    About PAGE - via upload

---
+++ About/script.js
@@ -0,0 +1,13 @@
+$(document).mousemove(function(e) {
+    $('.cursor1').offset({
+        left: e.pageX,
+        top: e.pageY + 20
+    });
+
+    $('.cursor2').offset({
+        left: e.pageX + 30,
+        top: e.pageY + 60
+    });
+
+
+});
